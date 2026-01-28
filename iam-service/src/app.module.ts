e5fe76d74b40529fe13484f88073c1a73d19c80b
Le Duc Anh Tuan <tuanleqn04@gmail.com>
2026-01-28 20:14:18 +0700

    feat: create signin, signup, refresh api

--- iam-service/src/app.module.ts
+++ iam-service/src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { ConfigModule } from '@nestjs/config/dist/config.module'
 
-@Module({ 
-  imports: [AuthModule, PrismaModule],
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PrismaModule,
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
-})  
+  providers: [AppService]
+})
 export class AppModule {}

59c8007922af746e7c16820dcd78c7d5e44ad2c7
sontapcode <156274946+tuitensonne@users.noreply.github.com>
2026-02-25 11:44:01 +0700

    Fixing bug on course-service

--- course-service/src/modules/course/course.module.ts
+++ course-service/src/modules/course/course.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { CourseController } from './course.controller'
 import { CourseService } from './course.service'
 import { PrismaService } from '../prisma/prisma.service'
+import { CourseRepositoy } from './course.repository'
 
 @Module({
   controllers: [CourseController],
-  providers: [CourseService, PrismaService],
+  providers: [CourseService, CourseRepositoy],
   exports: [CourseService]
 })
 export class CourseModule {}

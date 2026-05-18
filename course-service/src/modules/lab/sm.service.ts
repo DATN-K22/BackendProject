0516173581f7f1bd5b53361f7bbf510f91e713aa
sontapcode <156274946+tuitensonne@users.noreply.github.com>
2026-05-18 10:40:08 +0700

    Fixing on course service

--- course-service/src/modules/lab/sm.service.ts
+++ course-service/src/modules/lab/sm.service.ts
@@ -18,12 +18,8 @@ export class AwsSecretService implements OnModuleInit, ISecretManagementService 
 
   constructor(private readonly configService: ConfigService) {
     const region = configService.getOrThrow<string>('AWS_REGION')
-    const accessKeyId = configService.get<string>('AWS_ACCESS_KEY')
-    const secretAccessKey = configService.get<string>('AWS_SECRET_KEY')
 
-    const credentials = accessKeyId && secretAccessKey ? { accessKeyId, secretAccessKey } : undefined
-
-    this.client = new SecretsManagerClient({ region, credentials })
+    this.client = new SecretsManagerClient({ region })
   }
 
   async onModuleInit() {
